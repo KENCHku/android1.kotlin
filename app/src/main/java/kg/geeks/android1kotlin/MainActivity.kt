04a7d631ad3ab072c5aa829e39d4a7f02b456fba
KENCHku <kench.mkk3@gmail.com>
2023-05-03 01:37:25 +0600

    click, layoutInflater

--- app/src/main/java/kg/geeks/android1kotlin/MainActivity.kt
+++ app/src/main/java/kg/geeks/android1kotlin/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(parent))
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         navigateFragments()
